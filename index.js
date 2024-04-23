0ef99f93c067e8cbec4c25269eb5766e2a547623
MuhammadKhubaib0 <khubaibxheikh296@gmail.com>
2024-04-23 23:48:26 +0500

    Adding a cli nuber guessing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("welcome to Muhammad khubaib cli number guessing game");
+const RandomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Enter your guess number(number limit 1 to 5):",
+    }
+]);
+if (answer.userGuessedNumber === RandomNumber) {
+    console.log("Congratulation ! you guess a correct number");
+}
+else {
+    console.log("Sorry, you guess a wrong number");
+}
